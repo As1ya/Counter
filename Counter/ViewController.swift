ae69cd65ac0ff21473df27d4f4ea0db1fcec3091
As1ya <fed010va@mail.ru>
2025-11-07 13:57:46 +0300

    Выполнение базовой задачи

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NumberUILabel: UILabel!
+    
+    @IBOutlet weak var ChangeNumber: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NumberUILabel.text = "0"
     }
 
-
+    private var number: Int = 0
+    
+    
+    @IBAction func DidTabButton(_ sender: Any) {
+        number += 1
+        NumberUILabel.text = String(number)
+    }
+    
 }
 
