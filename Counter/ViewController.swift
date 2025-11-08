38db745c3d545ec3e78864e09828a1a2480ea747
As1ya <fed010va@mail.ru>
2025-11-08 10:04:28 +0300

    Усложненная задача. Финал

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,17 +13,60 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ChangeNumber: UIButton!
     
+    @IBOutlet weak var MinusNumber: UIButton!
+    
+    @IBOutlet weak var RestartUIButton: UIButton!
+    
+    @IBOutlet weak var HistoryUITextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NumberUILabel.text = "0"
+        HistoryUITextView.text = "История изменений"
+        ChangeNumber.tintColor = .systemRed
+        MinusNumber.tintColor = .systemBlue
     }
+    
+  
 
     private var number: Int = 0
+    private var isFirstAction = true
     
+    private func AddHistoryChanges(_ text: String){
+        let foramatter = DateFormatter()
+        foramatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let time = foramatter.string(from: Date())
+        
+        if isFirstAction {
+            HistoryUITextView.text = ""
+            isFirstAction = false
+        }
+        
+        HistoryUITextView.text = "[\(time)] \(text)\n " + HistoryUITextView.text
+    }
     
     @IBAction func DidTabButton(_ sender: Any) {
         number += 1
         NumberUILabel.text = String(number)
+        
+        AddHistoryChanges("значение изменено на +1")
+    }
+    
+    @IBAction func DidTabCancel(_ sender: Any) {
+        if number > 0 {
+            number -= 1
+            NumberUILabel.text = String(number)
+            AddHistoryChanges("значение изменено на -1")
+        } else {
+            AddHistoryChanges("попытка уменьшить значение счётчика ниже 0")
+        }
+        
+    }
+    
+    @IBAction func RestartNumber(_ sender: Any) {
+        number = 0
+        NumberUILabel.text = "0"
+        AddHistoryChanges("значение сброшено")
     }
     
 }
